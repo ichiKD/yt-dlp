d1bcf7c91e69cc011d2e63d90485773535d8cd18
Shubhang <94745178+ichiKD@users.noreply.github.com>
2024-05-11 11:38:06 +0530

    Removed requests module

--- yt_dlp/extractor/fansly.py
+++ yt_dlp/extractor/fansly.py
@@ -1,6 +1,8 @@
-import requests
 import datetime as dt
 import re
+import urllib.parse
+import urllib.request
+import json
 
 from .common import InfoExtractor
 from ..utils import (
@@ -17,8 +19,8 @@ class FanslyIE(InfoExtractor):
             'authority': 'apiv3.fansly.com',
             'accept': 'application/json, text/plain, */*',
             'accept-language': 'en;q=0.8,en-US;q=0.7',
-            # Apprently authorization token is not needed
-            # 'authorization': 'NjQxMzE0MzU3NjkwNTA3MjY0OjE6MjozNWJhMDVhMWVmY2M2ZjY4Njg2NGU5ZjY3NzMxOTc',  # Replace with your actual auth token
+            # Apprently authorization token is needed
+            'authorization': 'NjQxMzE0MzU3NjkwNTA3MjY0OjE6MjozNWJhMDVhMWVmY2M2ZjY4Njg2NGU5ZjY3NzMxOTc',  # Replace with your actual auth token
             'origin': 'https://fansly.com',
             'referer': 'https://fansly.com/',
             'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
@@ -38,13 +40,11 @@ class FanslyIE(InfoExtractor):
             stream_url = f'https://apiv3.fansly.com/api/v1/streaming/channel/{username}?ngsw-bypass=true'
         else:
             account_url = f'https://apiv3.fansly.com/api/v1/account?usernames={username}&ngsw-bypass=true'
-            user_Data = self._get_account_data(account_url)
-            stream_url = f'https://apiv3.fansly.com/api/v1/streaming/channel/{user_Data['response'][0]['id']}?ngsw-bypass=true'
+            user_data = self._get_account_data(account_url)
+            stream_url = f'https://apiv3.fansly.com/api/v1/streaming/channel/{user_data["response"][0]["id"]}?ngsw-bypass=true'
         data = self._get_stream_data(stream_url)
         print("The data is", data)
         formats = [{'url': traverse_obj(data, ('response', 'stream', 'playbackUrl')), }]
-        # if(traverse_obj(formats, 0, 'url') == None):
-        #     formats = [{'url': traverse_obj(data, ('response', 'stream', 'playbackUrl')), }]
         print({
             'id': traverse_obj(data, ('response', 'accountId')),
             'title': username,
@@ -68,61 +68,60 @@ class FanslyIE(InfoExtractor):
             return ""
 
     def _get_account_data(self, account_url):
-        response = requests.get(account_url, headers=self.header)
-        json_data = response.json()
-        if not dict_get(json_data, 'success') or len(dict_get(json_data, 'response')) == 0:
-            print('Error: could not retrieve account data')
-            exit()
-        metadata = {
-            "success": dict_get(json_data, 'success'),
-            "response": [{
-                "id": traverse_obj(json_data, ('response', 0, 'id')),
-                "username": traverse_obj(json_data, ('response', 0, 'username')),
-                "avatar": {
-                    "id": traverse_obj(json_data, ('response', 0, 'avatar', 'id')),
-                    "mimetype": traverse_obj(json_data, ('response', 0, 'avatar', 'mimetype')),
-                    "location": traverse_obj(json_data, ('response', 0, 'avatar', 'location')),
-                    "variants": [
-                        {
-                            "id": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'id')),
-                            "mimetype": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'mimetype')),
-                            "location": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'location')),
-                            "locations": [{
-                                "locationId": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'locations', 0, 'locationId')),
-                                "location": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'locations', 0, 'location')),
-                            }]
-                        }
-                    ]
-                }}]
-        }
-        return metadata
+        req = urllib.request.Request(account_url, headers=self.header)
+        with urllib.request.urlopen(req) as response:
+            json_data = json.load(response)
+            if not dict_get(json_data, 'success') or len(dict_get(json_data, 'response')) == 0:
+                print('Error: could not retrieve account data')
+                exit()
+            metadata = {
+                "success": dict_get(json_data, 'success'),
+                "response": [{
+                    "id": traverse_obj(json_data, ('response', 0, 'id')),
+                    "username": traverse_obj(json_data, ('response', 0, 'username')),
+                    "avatar": {
+                        "id": traverse_obj(json_data, ('response', 0, 'avatar', 'id')),
+                        "mimetype": traverse_obj(json_data, ('response', 0, 'avatar', 'mimetype')),
+                        "location": traverse_obj(json_data, ('response', 0, 'avatar', 'location')),
+                        "variants": [
+                            {
+                                "id": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'id')),
+                                "mimetype": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'mimetype')),
+                                "location": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'location')),
+                                "locations": [{
+                                    "locationId": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'locations', 0, 'locationId')),
+                                    "location": traverse_obj(json_data, ('response', 0, 'avatar', 'variants', 0, 'locations', 0, 'location')),
+                                }]
+                            }
+                        ]
+                    }}]
+            }
+            return metadata
 
     def _get_stream_data(self, stream_url):
-        response = requests.get(stream_url, headers=self.header)
-        data = response.json()
-        last_fetched = traverse_obj(data, ('response', 'stream', 'lastFetchedAt'))
-        current_time = int(dt.datetime.now().timestamp() * 1000)
-        # import json
-        # print("The stream data is:\n")
-        # print(json.dumps(data, indent=4))
-        playbackUrl = traverse_obj(data, ('response', 'playbackUrl'))
-        if current_time - last_fetched > 4 * 60 * 1000 or playbackUrl is False:
-            return {"success": False, "response": None}
-        else:
-            metadata = {
-                "success": dict_get(data, 'success'),
-                "response": {
-                    "id": traverse_obj(data, ('response', 'id')),
-                    "accountId": traverse_obj(data, ('response', 'accountId')),
-                    "playbackUrl": traverse_obj(data, ('response', 'playbackUrl')),
-                    "stream": {
-                        "id": traverse_obj(data, ('response', 'stream', 'id')),
-                        "title": traverse_obj(data, ('response', 'stream', 'title')),
-                        "status": traverse_obj(data, ('response', 'stream', 'status')),
-                        "lastFetchedAt": traverse_obj(data, ('response', 'stream', 'lastFetchedAt')),
-                        "startedAt": traverse_obj(data, ('response', 'stream', 'startedAt')),
-                        "playbackUrl": traverse_obj(data, ('response', 'stream', 'playbackUrl')),
-                    },
+        req = urllib.request.Request(stream_url, headers=self.header)
+        with urllib.request.urlopen(req) as response:
+            data = json.load(response)
+            last_fetched = traverse_obj(data, ('response', 'stream', 'lastFetchedAt'))
+            current_time = int(dt.datetime.now().timestamp() * 1000)
+            playbackUrl = traverse_obj(data, ('response', 'playbackUrl'))
+            if current_time - last_fetched > 4 * 60 * 1000 or playbackUrl is False:
+                return {"success": False, "response": None}
+            else:
+                metadata = {
+                    "success": dict_get(data, 'success'),
+                    "response": {
+                        "id": traverse_obj(data, ('response', 'id')),
+                        "accountId": traverse_obj(data, ('response', 'accountId')),
+                        "playbackUrl": traverse_obj(data, ('response', 'playbackUrl')),
+                        "stream": {
+                            "id": traverse_obj(data, ('response', 'stream', 'id')),
+                            "title": traverse_obj(data, ('response', 'stream', 'title')),
+                            "status": traverse_obj(data, ('response', 'stream', 'status')),
+                            "lastFetchedAt": traverse_obj(data, ('response', 'stream', 'lastFetchedAt')),
+                            "startedAt": traverse_obj(data, ('response', 'stream', 'startedAt')),
+                            "playbackUrl": traverse_obj(data, ('response', 'stream', 'playbackUrl')),
+                        },
+                    }
                 }
-            }
-        return metadata
+                return metadata
